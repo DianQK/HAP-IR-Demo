bcf7038d837b56c5a65082ae25624cc6afd0ecf1
DianQK <dianqk@icloud.com>
2019-09-14 12:52:01 +0800

    打印时间差值

--- Sources/HAP-IR-Demo/main.swift
+++ Sources/HAP-IR-Demo/main.swift
@@ -9,8 +9,21 @@ import Darwin.C
 let gpios = SwiftyGPIO.GPIOs(for:.RaspberryPi3)
 let gp = gpios[.pin18]!
 gp.direction = .input
+var preTime: UInt64 = 0
+var times: [UInt64] = []
 gp.onChange { (gpio) in
-    print(gpio.value)
+    if (preTime == 0 && !gpio.value) { // 首个信号为脉冲，即 value 从 false 变 true 开始计算
+        preTime = DispatchTime.now().uptimeNanoseconds
+        print("收到信号，1s 后打印结果")
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1, execute: {
+            print(times)
+            preTime = 0
+            times = []
+        })
+    } else if (preTime > 0) {
+        let now = DispatchTime.now().uptimeNanoseconds
+        times.append((now - preTime) / 1000)
+        preTime = now
+    }
 }
-
 RunLoop.main.run()
